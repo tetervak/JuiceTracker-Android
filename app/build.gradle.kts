f14f53b599b550f67a5b2823c1519cd842e0f2a8
Alex Tetervak <tetervak@sheridancollege.ca>
2023-12-03 18:04:27 -0500

    updated to the latest compose BOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
@@ -67,7 +67,7 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
