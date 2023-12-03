373da34f1268980b44af1222cbfd5722a80015c8
Alex Tetervak <tetervak@sheridancollege.ca>
2023-12-03 18:33:10 -0500

    updated libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,14 @@
 
 buildscript {
     extra.apply {
-        set("nav_version", "2.6.0")
-        set("room_version", "2.5.2")
-        set("arch_lifecycle_version", "2.6.1")
+        set("nav_version", "2.7.5")
+        set("room_version", "2.6.1")
+        set("arch_lifecycle_version", "2.6.2")
     }
 }
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("androidx.navigation.safeargs") version "2.6.0" apply false
 }
